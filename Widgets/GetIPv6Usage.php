3b788f2b2b0f0d0c292a88c0f93e909927effa1b
Johannes Singler <johannes@singler.name>
2017-04-15 18:26:24 +0200

    Replace widgets by ones compatible to Piwik 3 (only).  New major version for plugin as recommended.

---
+++ Widgets/GetIPv6Usage.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\IPv6Usage\Widgets;
+
+use Piwik\Widget\Widget;
+use Piwik\Widget\WidgetConfig;
+use Piwik\View;
+
+/**
+ * This class allows you to add your own widget to the Piwik platform. In case you want to remove widgets from another
+ * plugin please have a look at the "configureWidgetsList()" method.
+ * To configure a widget simply call the corresponding methods as described in the API-Reference:
+ * http://developer.piwik.org/api-reference/Piwik/Plugin\Widget
+ */
+class GetIPv6Usage extends Widget
+{
+    public static function configure(WidgetConfig $config)
+    {
+        /**
+         * Set the category the widget belongs to. You can reuse any existing widget category or define
+         * your own category.
+         */
+        $config->setCategoryId('General_Visitors');
+
+        /**
+         * Set the subcategory the widget belongs to. If a subcategory is set, the widget will be shown in the UI.
+         */
+        $config->setSubcategoryId('IPv6Usage_IPv6Usage');
+
+        /**
+         * Set the name of the widget belongs to.
+         */
+        $config->setName('IPv6Usage_IPv6Usage');
+
+        /**
+         * Set the order of the widget. The lower the number, the earlier the widget will be listed within a category.
+         */
+        $config->setOrder(1);
+    }
+
+    /**
+     * This method renders the widget. It's on you how to generate the content of the widget.
+     * As long as you return a string everything is fine. You can use for instance a "Piwik\View" to render a
+     * twig template. In such a case don't forget to create a twig template (eg. myViewTemplate.twig) in the
+     * "templates" directory of your plugin.
+     *
+     * @return string
+     */
+    public function render()
+    {
+        $controller = new \Piwik\Plugins\IPv6Usage\Controller();
+        return $controller->getIPv6UsageGraph();
+    }
+}
